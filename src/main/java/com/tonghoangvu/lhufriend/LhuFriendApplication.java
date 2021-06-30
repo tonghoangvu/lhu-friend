36cab919e8d2fc66b36b3ae6edd1818daec8b95c
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-06-30 18:27:28 +0700

    feat: init project

---
+++ src/main/java/com/tonghoangvu/lhufriend/LhuFriendApplication.java
@@ -0,0 +1,13 @@
+package com.tonghoangvu.lhufriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhuFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhuFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonghoangvu/lhufriend/LhuFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonghoangvu.lhufriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhuFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
