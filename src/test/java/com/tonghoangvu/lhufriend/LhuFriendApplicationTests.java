6f41e8d8d0dd2bbb931577844933de0acd62f277
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-06-30 18:39:44 +0700

    style: fix coding style

--- src/test/java/com/tonghoangvu/lhufriend/LhuFriendApplicationTests.java
+++ src/test/java/com/tonghoangvu/lhufriend/LhuFriendApplicationTests.java
@@ -5,6 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LhuFriendApplicationTests {
-	@Test
-	void contextLoads() {}
+    @Test
+    void contextLoads() {}
 }
